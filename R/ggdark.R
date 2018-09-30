589b8477e9a47406f7915101b2f19a1bdaa81945
Neal Grantham <neal@nsgrantham.com>
2018-09-30 01:06:18 +0000

    Add examples for every function

--- R/ggdark.R
+++ R/ggdark.R
@@ -5,12 +5,41 @@
 #' @param geom_fill Default fill value for ggplot2::geom_*
 #' @param geom_color Default color value for ggplot2::geom_*
 #' @param geom_colour Alias of geom_color
+#' @param update_geoms Overwrite default fill and color values for ggplot2::geom_*?
+#'
 #' @return Theme with dark mode elements
+#'
 #' @importFrom ggplot2 theme theme_get element_rect element_line element_text
 #' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' p1 <- ggplot(iris, aes(Sepal.Width, Sepal.Length)) +
+#' geom_point(aes(color = Species))
+#'
+#' p1  # theme_gray(), the default
+#' p1 + darken()  # activate dark mode on theme_gray()
+#' p1 + darken(theme_minimal())  # darken a different ggplot2 theme
+#' p1 + darken(theme_minimal(), background = "grey20")  # lighten black background
+#'
+#' p2 <- ggplot(iris, aes(Sepal.Width, Sepal.Length)) +
+#'   geom_point() +
+#'   facet_wrap(~ Species)
+#'
+#' p2 + darken()  # geom color is now white
+#' p2 + darken(geom_color = "grey80")  # or perhaps a light grey
+#'
+#' # you can update the geoms outside of darken, but make
+#' # sure they're not overwritten when darken is run again
+#' update_geom_colors(color = "orange", fill = "gold")
+#' p2 + darken(update_geoms = FALSE)
+#'
+#' restore_geom_colors()  # restore defaults fill = "black", color = "black"
+#'
 #' @rdname darken
 darken <- function(.theme = ggplot2::theme_get(), background = "black", geom_fill = "white",
-                   geom_color = "white", geom_colour = geom_color) {
+                   geom_color = "white", geom_colour = geom_color, update_geoms = TRUE) {
   stopifnot(all(class(.theme) == c("theme", "gg")))
   element_names <- names(.theme)
   updated_elements <- list()
@@ -33,7 +62,9 @@ darken <- function(.theme = ggplot2::theme_get(), background = "black", geom_fil
     }
   }
 
-  update_geom_colors(fill = geom_fill, color = geom_color)
+  if (update_geoms) {
+    update_geom_colors(fill = geom_fill, color = geom_color)
+  }
 
   .theme +
     do.call("theme", updated_elements) +  # calls ggplot2::theme()
@@ -45,9 +76,29 @@ darken <- function(.theme = ggplot2::theme_get(), background = "black", geom_fil
 #' @param fill Default fill value for ggplot2::geom_*
 #' @param color Default color value for ggplot2::geom_*
 #' @param colour Alias of color
-#' @seealso [update_geom_colours()]
+#'
 #' @importFrom ggplot2 update_geom_defaults
 #' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' p2 <- ggplot(iris, aes(Sepal.Width, Sepal.Length)) +
+#'   geom_point() +
+#'   facet_wrap(~ Species)
+#'
+#' # geom colors are updated by ggdark::darken if its
+#' # update_geoms arg is TRUE
+#' p2 + darken()  # update_geoms = TRUE by default
+#'
+#' # you can update the geoms outside of darken, but make
+#' # sure they're not overwritten when darken is run again
+#' update_geom_colors(color = "orange", fill = "gold")
+#' p2 + darken(update_geoms = FALSE)
+#'
+#' restore_geom_colors()  # update_geom_colors(fill = "black", color = "black")
+#'
+#' @seealso [update_geom_colours()]
 #' @rdname update_geom_colors
 update_geom_colors <- function(fill, color, colour = color) {
   geoms <- c("abline", "area", "bar", "boxplot", "col", "crossbar",
@@ -68,6 +119,20 @@ update_geom_colours <- update_geom_colors
 #' Restore the geom defaults of fill = "white and color = "black"
 #'
 #' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' p2 <- ggplot(iris, aes(Sepal.Width, Sepal.Length)) +
+#'   geom_point() +
+#'   facet_wrap(~ Species)
+#'
+#' p2 + darken()  # updates geom fill = "white", color = "white"
+#' p2  # oh no! we can't see the points anymore because they are white on white
+#' restore_geom_colors()
+#' p2  # back to normal
+#'
+#' @seealso [restore_geom_colours()]
 #' @rdname restore_geom_colors
 restore_geom_colors <- function() update_geom_colors(fill = "black", color = "black")
 
@@ -79,7 +144,21 @@ restore_geom_colours <- restore_geom_colors
 #' Get greyscale complement
 #'
 #' @param color Greyscale color to complement
+#'
+#' @return Complementary greyscale color
+#'
 #' @export
+#'
+#' @examples
+#' greyscale_complement("white")    # "black"
+#' greyscale_complement("black")    # "white"
+#' greyscale_complement("gray20")   # "gray80"
+#' greyscale_complement("grey20")   # "grey80"
+#' greyscale_complement("indigo")   # "indigo" (not on greyscale, so not complemented)
+#' greyscale_complement("#000000")  # "#FFFFFF"
+#' greyscale_complement("#333333")  # "#CCCCCC"
+#' greyscale_complement("#4B0082")  # "#4B0082" (not on greyscale, so not complemented)
+#'
 #' @rdname greyscale_complement
 greyscale_complement <- function(color) {
   if (is.null(color) || is.na(color)) {
@@ -94,9 +173,16 @@ greyscale_complement <- function(color) {
 
 #' Get greyscale complement to color by hexcode
 #'
-#' @param color Greyscale color to complement
+#' @param color Greyscale color to complement (hexcode)
+#'
+#' @return Complementary greyscale color (hexcode)
+#'
 #' @importFrom grDevices col2rgb rgb
-#' @export
+#'
+#' @examples
+#' # Wrapped by greyscale_compelement
+#' greyscale_complement("#333333")  # calls greyscale_complement_hex
+#'
 #' @rdname greyscale_complement_hex
 greyscale_complement_hex <- function(color) {
   color_rgb <- c(grDevices::col2rgb(color))
@@ -111,8 +197,14 @@ greyscale_complement_hex <- function(color) {
 
 #' Get greyscale complement to color by name
 #'
-#' @param color Greyscale color to complement
-#' @export
+#' @param color Greyscale color to complement (name)
+#'
+#' @return Complementary greyscale color (name)
+#'
+#' @examples
+#' # Wrapped by greyscale_compelement
+#' greyscale_complement("grey80")  # calls greyscale_complement_name
+#'
 #' @rdname greyscale_complement_name
 greyscale_complement_name <- function(color) {
   if (color == "black") {
